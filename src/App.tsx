d9cdf1ab158a4f97be6443f401c52ce524952820
Tomek <tomek@el-jot.eu>
2023-11-19 20:22:53 +0100

    added preview, empty repo to pre main branch

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import styles from "./styles.module.scss"
+
+function App() {
+
+  return (
+    <div className={styles.div_test1}>
+        <h1>Test1</h1>
+        <div className={styles.div_test2}>
+            <h2>Test2</h2>
+        </div>
+    </div>
+  )
+}
+
+export default App
