fa277ccd62df5cae549682dd08fb2aca1e4fa00c
Kuba <jakubkulik20@gmail.com>
2023-11-25 17:49:43 +0100

    Fix #1

--- Backend/User/get_data.py
+++
@@ -1,9 +0,0 @@
-import models
-from sqlalchemy.orm import sessionmaker
-
-Session = sessionmaker(bind=models.engine)
-session = Session()
-
-for row in session.query(models.User).all():
-    print(row.user_id, row.email, row.name, row.surname, row.phone_number, row.password)
-
