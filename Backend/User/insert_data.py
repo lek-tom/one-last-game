0f88daa3daa455e2e6ebb39c3cdaa96172c06be7
Jakub <jakubkulik20@gmail.com>
2024-01-03 12:53:49 +0100

    fix

--- Backend/User/insert_data.py
+++ Backend/User/insert_data.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 from fastapi import FastAPI, Depends, Request
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.middleware.cors import CORSMiddleware
@@ -21,7 +23,7 @@ app.add_middleware(
 @app.post("/insert_data")
 async def create_user(user: Request, db: AsyncSession = Depends(models.get_db)):
     print(user)
-    db_user = models.User(email=user.get("email"), name=user.get("name"), surname=user.get("surname"), password=user.get("password"), phone_number=user.get("phone_number"), sex=user.get("sex"))
+    db_user = models.User(user_id=uuid4, email=user.get("email"), name=user.get("name"), surname=user.get("surname"), password=user.get("password"), phone_number=user.get("phone_number"), sex=user.get("sex"))
     db.add(db_user)
     await db.commit()
     await db.refresh(db_user)
